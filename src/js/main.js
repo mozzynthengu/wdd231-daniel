411f1496cd322653108904f3155fecf1c5b5da42
Mosiah Nthengu <mosiah.nthengu@gmail.com>
2026-04-25 17:24:52 +0200

    Cleaned files

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+// Disclaimer
+const disclaimer = document.querySelector(".disclaimer a");
+disclaimer.href = parkData.url;
+disclaimer.textContent = parkData.fullName;
+
+// Page title
+document.title = parkData.fullName;
+
+// Hero image
+const heroImg = document.querySelector(".hero-banner img");
+heroImg.src = parkData.images[0].url;
+heroImg.alt = parkData.images[0].altText;
+
+// Park info
+function parkInfoTemplate(info) {
+  return `
+    <a href="#" class="hero-banner__title">${info.fullName}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>
+  `;
+}
+
+const heroContent = document.querySelector(".hero-banner__content");
+heroContent.innerHTML = parkInfoTemplate(parkData);
